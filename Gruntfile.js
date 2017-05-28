020bc3f2a6bf288cbe4d32184db7d50d8081c9f5
Jakub <jakub.kraska95@gmail.com>
2017-05-28 22:45:36 +0200

    cssmin task included

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,87 @@
-module.exports = function(grunt) {
-grunt.initConfig({
-  pkg: grunt.file.readJSON('package.json'),
-    sass: {
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Tasks
+    sass: { // Begin Sass Plugin
       dist: {
         options: {
-          sourcemap: false,
-          compress: false,
-          yuicompress: false,
-          style: 'expanded',
+          sourcemap: 'none'
         },
-        files: {
-          'css/style.css' : 'sass/style.scss'
-          //lewo: plik ktory bedzie nam ze wszystkiego wychodzil
-          //prawo w tym folderze ma byc style.scss, powiem wiecej
-        }
-      },
+        files: [{
+          expand: true,
+          cwd: 'sass',
+          src: ['**/*.scss'],
+          dest: 'css',
+          ext: '.css'
+      }]
+      }
     },
-    watch: {
+    cssmin: { // Begin CSS Minify Plugin
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+    }]
+      }
+    },
+    //uglify: { // Begin JS Uglify Plugin
+    //  build: {
+    //    src: ['src/*.js'],
+    //    dest: 'js/script.min.js'
+    //  }
+    //},
+    watch: { // Compile everything into one task with Watch Plugin
       css: {
         files: '**/*.scss',
-        tasks: ['sass']
+        tasks: ['sass', 'cssmin']
+      },
+      js: {
+        files: '**/*.js',
+        tasks: ['uglify']
       }
     }
   });
-grunt.loadNpmTasks('grunt-contrib-sass');
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.registerTask('default',['watch']);
-}
+  // Load Grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Register Grunt tasks
+  grunt.registerTask('default', ['watch']);
+};
+
+
+
+
+// module.exports = function(grunt) {
+// grunt.initConfig({
+//   pkg: grunt.file.readJSON('package.json'),
+//     sass: {
+//       dist: {
+//         options: {
+//           sourcemap: false,
+//           compress: false,
+//           yuicompress: false,
+//           style: 'expanded',
+//         },
+//         files: {
+//           'css/style.css' : 'sass/style.scss'
+//         }
+//       },
+//     },
+//     watch: {
+//       css: {
+//         files: '**/*.scss',
+//         tasks: ['sass']
+//       }
+//     }
+//   });
+// grunt.loadNpmTasks('grunt-contrib-sass');
+// grunt.loadNpmTasks('grunt-contrib-watch');
+// grunt.registerTask('default',['watch']);
+// }
