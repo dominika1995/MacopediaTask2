c906162510e1575f0d81ffa3a5114440eaf3bdf3
Jakub <jakub.kraska95@gmail.com>
2017-06-17 11:32:02 +0200

    some impovements in first sections

--- js/accordion.js
+++ js/accordion.js
@@ -4,19 +4,17 @@ $( document ).ready(function() {
     var width = $(window).width();
     if((width>992) && (width<1200)){
       var maxHeight = height - 230;
-      $('.panel-body').css('max-height', `${maxHeight}px`);
+      $('.panel-body').css('max-height', maxHeight + "px");
     }
     else{
       $('.panel-body').css('max-height', '200px');
     }
   });
-
   $('.panel-heading a').on('click',function(e){
     if($(this).parents('.panel').children('.panel-collapse').hasClass('in')){
         e.preventDefault();
         e.stopPropagation();
     }
 });
-
   window.dispatchEvent(new Event('resize'));
 });
