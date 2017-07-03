ff4ecda12c30d402bd0a9d5322b8191cf4eeaf31
Jakub <jakub.kraska95@gmail.com>
2017-07-03 16:20:47 +0200

    fixed menu

--- js/nav.js
+++ js/nav.js
@@ -5,6 +5,10 @@ $( document ).ready(function() {
         $("#nav-control").toggleClass("menu-open");
         $( ".nav-control > .wrapper" ).slideToggle( "slow" );
         e.stopPropagation();
+        if ($("#nav-control").offset().top < 50){
+            $("#nav-control").toggleClass("scrolled");
+            $(".logo").toggleClass("scrolled");
+        }
         return false;
     });
     $('.link>.page-scroll').click(function(e){
@@ -19,6 +23,20 @@ $( document ).ready(function() {
             $("#nav-control").toggleClass("menu-open");
             $( ".nav-control > .wrapper" ).slideToggle( "slow" );
         }
+
+    });
+
+    $(window).scroll(function() {
+        if(!hamburger.hasClass('active')){
+            if ($("#nav-control").offset().top > 50) {
+                $("#nav-control").addClass("scrolled");
+                $(".logo").addClass("scrolled");
+            } else {
+                $("#nav-control").removeClass("scrolled");
+                $(".logo").removeClass("scrolled");
+            }
+        }
+
 
     });
 });
