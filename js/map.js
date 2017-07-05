7e986d53c03540faa7d725241482de1c9f0dcdf5
dominika15 <dominikaa.b@onet.pl>
2017-07-05 12:28:20 +0200

    Fix map and function in map.js

--- js/map.js
+++ js/map.js
@@ -1,46 +1,45 @@
-var modal = document.getElementById('box');
+$(document).ready(function () {
+    var modal = document.getElementById('box');
 
-var btn = document.getElementById("myBtn");
+    var btn = document.getElementById("myBtn");
 
-var span = document.getElementsByClassName("map__box--content--close")[0];
+    var span = document.getElementsByClassName("map__box--content--close")[0];
 
-btn.onclick = function() {
-    modal.style.display = "block";
-    getLocation();
-}
+    btn.onclick = function () {
+        modal.style.display = "block";
+        getLocation();
+    }
 
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
+    span.onclick = function () {
         modal.style.display = "none";
     }
-}
 
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        alert("Geolocation is not supported by this browser.");
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
-}
 
-function showPosition(position) {
-    var lat=position.coords.latitude
-    var lng = position.coords.longitude;
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            alert("Geolocation is not supported by this browser.");
+        }
+    }
 
-    var posit = {lat: lat, lng:lng};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: posit
-    });
+    function showPosition(position) {
+        var lat = position.coords.latitude
+        var lng = position.coords.longitude;
+        var posit = {lat: lat, lng: lng};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 10,
+            center: posit
+        });
+        var marker = new google.maps.Marker({
+            position: posit,
+            map: map,
+        });
 
-    var marker = new google.maps.Marker({
-        position: posit,
-        map: map,
-    });
-
-}
+    }
+});
